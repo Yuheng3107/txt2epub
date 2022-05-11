b0971f58d1aab94290250ec7ee9d72d2bd65a17a
Kuang Yu Heng <yuheng3107@gmail.com>
2022-05-11 15:25:33 +0800

    Edited code to correct & sign

---
+++ Ultimate AWS Certified Solutions Architect Associate 2022_files/udlite-common-css.82d96af66b05db156df0.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["udlite-common-css"],[]]);
+//# sourceMappingURL=udlite-common-css.82d96af66b05db156df0.js.map
--- parser.py
+++ parser.py
@@ -2,19 +2,32 @@ import re
 import os
 import chardet
 import codecs
-
-def check_encoding(book_name):
-    with open(book_name, "rb") as f:
-        data = f.read(100)
-        encoding = chardet.detect(data)['encoding']
-    if encoding != 'utf-8':
-        correct_encoding(book_name)
 def correct_encoding(book_name):
     with codecs.open(book_name, "r", "gb18030") as f:
         data = f.read()
         data.replace("&", "&amp")
         with codecs.open(book_name, "w", "utf-8") as newfile:
             newfile.write(data)
+
+
+def check_encoding(book_name):
+    with open(book_name, "rb") as f:
+        data = f.read(100)
+        encoding = chardet.detect(data)['encoding']
+        if encoding != 'utf-8':
+            correct_encoding(book_name)
+            return
+    with open(book_name, "r") as file:
+        ### Cleaning out & which is forbidden in XML
+        data = file.read()
+        data.replace("&", "&amp")
+        with open(book_name, "w") as newfile:
+            newfile.write(data)  
+             
+            
+
+
+
 
 
 
