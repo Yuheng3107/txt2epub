a0a7c00f0b71961459ef3559b97f4df4d0b2c7f9
Kuang Yu Heng <yuheng3107@gmail.com>
2022-05-21 16:54:26 +0800

    Added cleaning of forbidden chars to parser.py

--- parser.py
+++ parser.py
@@ -5,24 +5,32 @@ import codecs
 def correct_encoding(book_name):
     with codecs.open(book_name, "r", "gb18030") as f:
         data = f.read()
-        data.replace("&", "&amp")
+        data = data.replace("&", "&amp;")
+        data = data.replace("<", "&lt;")
+        data = data.replace(">", "&gt;")
         with codecs.open(book_name, "w", "utf-8") as newfile:
             newfile.write(data)
 
 
 def check_encoding(book_name):
     with open(book_name, "rb") as f:
+        print('test')
+        return
         data = f.read(100)
+        print(data)
         encoding = chardet.detect(data)['encoding']
+        print(encoding)
         if encoding != 'utf-8':
             correct_encoding(book_name)
             return
     with open(book_name, "r") as file:
-        ### Cleaning out & which is forbidden in XML
+        ### Cleaning out things which are forbidden in XML
         data = file.read()
-        data.replace("&", "&amp")
+        data = data.replace("&", "&amp;")
+        data = data.replace("<", "&lt;")
+        data = data.replace(">", "&gt;")
         with open(book_name, "w") as newfile:
-            newfile.write(data)  
+            newfile.write(data) 
              
             
 
